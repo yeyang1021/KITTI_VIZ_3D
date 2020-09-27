fe2dd9442649132fbdc656220767e508a6bc5f20
yeyang1021 <366175300@qq.com>
2020-09-27 09:31:40 +0800

    Update draw_box_Lidar.py

--- draw_box_Lidar.py
+++ draw_box_Lidar.py
@@ -40,7 +40,9 @@ def compute_3d_box_cam2(h, w, l, x, y, z, yaw):
     """
     Return : 3xn in cam2 coordinate
     """
-    R = np.array([[np.cos(yaw), 0, np.sin(yaw)], [0, 1, 0], [-np.sin(yaw), 0, np.cos(yaw)]])
+    
+    R = np.array([[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
+
 
     x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2]
     #y_corners = [h/2,h/2,h/2,h/2,-h/2,-h/2,-h/2,-h/2]
